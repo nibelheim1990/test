b9759d644ff3925ca8de6c96bea96e1908863068
Vladimir Ponomarev <trahti@rambler.ru>
2015-07-05 01:58:58 +0600

    Simple Autorization

---
+++ Transport/src/main/java/com/itapteka/transport/jspcontroller/MainController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itapteka.transport.jspcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Ponomarev
+ */
+@Controller
+public class MainController {
+    
+    @RequestMapping(method =RequestMethod.GET,value = "/offlinehistory")
+    public ModelAndView OfflineHistory(){
+    
+        System.out.println("OfflineHistory");
+        ModelAndView mav = new ModelAndView("offlinehistory");
+        return mav;
+    }
+}
---
+++ Transport/src/main/webapp/WEB-INF/jsp/offlinehistory.jsp
@@ -0,0 +1,12 @@
+<%-- 
+    Document   : offlinehistory
+    Created on : 05.07.2015, 1:07:03
+    Author     : Ponomarev
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<div class="container">
+    <div class="starter-template">
+        <h1>oflineHistory</h1>   
+    </div>
+</div>
